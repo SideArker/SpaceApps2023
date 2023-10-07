029c71e791053497944474430da7f6fd1b7971f3
LemonkaPix <maciejwrobel029@gmail.com>
2023-10-07 15:16:25 +0200

    prototype miner

---
+++ Assets/Game/Scripts/Drill.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drill : MonoBehaviour
+{
+    public bool IsTrigger = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IsTrigger = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IsTrigger = false;
+    }
+}
