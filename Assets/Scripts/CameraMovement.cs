5fa0fd7a64410193362dd40b1b5d3bfac6b29067
Kapelanq <pixel131513@gmail.com>
2023-10-07 21:13:04 +0200

    Camera locked in bounds
    now camera properly locks in bounds of the map, finally kurwa

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,24 +4,80 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    public float speed = 5.0f;
-    void Update()
+    public float cameraSpeed = 5f;
+    public float offset = 0.1f;
+
+    public bool isAtTopWall = true;
+    public bool isAtBottomWall = false;
+
+    public float topWallPosition = -4.42f;
+    public float bottomWallPosition = -11.98f;
+
+    public bool isAtRightWall = false;
+    public bool isAtLeftWall = false;
+
+    public float rightWallPosition = 38.28f;
+    public float leftWallPosition = -16.27f;
+
+    void CameraMoveVertical()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        
+
+        if (transform.position.y >= topWallPosition)
+            isAtTopWall = true;
+        else 
+            isAtTopWall = false;
+        
+        if(transform.position.y <= bottomWallPosition)
+            isAtBottomWall = true;
+        else
+            isAtBottomWall = false;
+
+        if (Input.GetKey(KeyCode.W) && !isAtTopWall)
         {
-            transform.position = new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.position += transform.up * cameraSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.S) && !isAtBottomWall)
         {
-            transform.position = new Vector3(speed * Time.deltaTime, 0, 0);
+            transform.position -= transform.up * cameraSpeed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position = new Vector3(0, speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            transform.position = new Vector3(0, speed * Time.deltaTime, 0);
-        }
+
+
     }
+
+    void CameraMoveHorizontal()
+    {
+
+      
+
+        if (transform.position.x >= rightWallPosition)
+            isAtRightWall = true;
+        else
+            isAtRightWall = false;
+
+        if(transform.position.x <= leftWallPosition)
+            isAtLeftWall = true;
+        else
+            isAtLeftWall = false;
+
+        if (Input.GetKey(KeyCode.D) && !isAtRightWall)
+        { 
+            transform.position += transform.right * cameraSpeed * Time.deltaTime;
+        }
+        else if(Input.GetKey(KeyCode.A ) && !isAtLeftWall)
+        {
+            transform.position -= transform.right * cameraSpeed * Time.deltaTime;
+        }
+           
+    }
+
+
+    void FixedUpdate()
+    {
+   
+        CameraMoveHorizontal();
+        CameraMoveVertical();
+       
+    }
+
 }
