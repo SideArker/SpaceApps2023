58089cac8e9463d822a10ed08f3a7e7106a7bc6d
SideArker <rsowa126@gmail.com>
2023-10-07 17:34:20 +0200

    Astronaut select

--- Assets/MapSelection/Scripts/UIController.cs
+++ Assets/MapSelection/Scripts/UIController.cs
@@ -3,13 +3,19 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class ColorChange : MonoBehaviour
+public class UIController : MonoBehaviour
 {
     [SerializeField] Color baseColor;
     [SerializeField] Color litColor;
-
+    [SerializeField] Animator FadeIn;
     public void ColorSwitch(TMP_Text text)
     {
         text.color = text.color == baseColor ? litColor : baseColor;
+    }
+
+    private void Start()
+    {
+        FadeIn.gameObject.SetActive(true);
+        FadeIn.Play("ScreenFadeIn");
     }
 }
