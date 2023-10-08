25f4f6d223c572273db867e700fd515eacbe5cc5
SideArker <rsowa126@gmail.com>
2023-10-08 02:06:44 +0200

    quests and skillchecks

---
+++ Assets/Game/Scripts/UI/QuestController.cs
@@ -0,0 +1,34 @@
+using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestController : MonoBehaviour
+{
+    public static QuestController instance;
+    [SerializeField] bool[] questState = new bool[5];
+    [SerializeField] GameObject[] questObjects = new GameObject[5];
+    [SerializeField] GameObject WinScreen;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void ChangeQuestState(int questID)
+    {
+        questState[questID] = true;
+        questObjects[questID].GetComponent<Toggle>().isOn = true;
+        questObjects[questID].GetComponentInChildren<TMP_Text>().fontStyle = FontStyles.Strikethrough;
+
+        if (questState.Any(x => x == false)) return;
+
+        // All quests completed
+
+        Debug.Log("Quests completed. Mision finished");
+
+    }
+}
